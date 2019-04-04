810fa53d4e776fa9b79e837ffdeeacdf5f27a513
Pepe Doval <pepellou@gmail.com>
2019-04-04 18:34:10 -0500

    Moderate bad words
    Detects users who say bad words, and counts strikes for them.
    After 3rd strike, times out user for 2 minutes.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var tmi = require('tmi.js');
 
+var bad_words_timeout_seconds = 120;
+
 var channels = [ 'pepellou' ];
 
 var options = {
@@ -17,6 +19,47 @@ var options = {
 
 var client = new tmi.client(options);
 client.connect();
+
+var bad_words = [
+    'fuck',
+    'fuckk',
+    'fuckkk',
+    'fuckkkk',
+    'fuckkkkk',
+    'fuckkkkkk',
+    'ffuck',
+    'mor0n',
+    'm0ron',
+    'm0r0n',
+    'moron'
+];
+
+var bad_words_strikes = { };
+
+client.on('chat', function(channel, userstate, message, self) {
+    var username = userstate['display-name'];
+    if (message
+        .toLowerCase()
+        .split('')
+        .filter(_ => (_ == ' ') || (_ >= '0' && _ <= '9') || (_ >= 'a' && _ <= 'z') )
+        .join('')
+        .split(' ')
+        .some(_ => bad_words.includes(_))
+    ) {
+        if (!bad_words_strikes[username]) {
+            bad_words_strikes[username] = 0;
+        }
+        bad_words_strikes[username]++;
+        var strikes = bad_words_strikes[username];
+        if (strikes < 3) {
+            client.say(channel, 'grrrr @' + username + ', this is your strike number ' + strikes + '. With 3 strikes you\'ll be timed out.');
+        } else {
+            client.say(channel, 'grrrr @' + username + ', this was your strike number ' + strikes + '. You are timed out.');
+            client.timeout(channel, username, bad_words_timeout_seconds, "Using bad words");
+            bad_words_strikes[username] = 0;
+        }
+    }
+});
 
 client.on('connected', function(address, port) {
     channels.forEach(_ => client.say(_, 'Hey there! HeyGuys'));
